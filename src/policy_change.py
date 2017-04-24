2bdd517281d2a5f3cfab685db6328e50111b796f
ajayaadhikari <A.Adhikari@student.tudelft.nl>
2017-04-24 18:16:51 +0200

    added global change

--- src/policy_change.py
+++ src/policy_change.py
@@ -10,7 +10,7 @@ fileName = "../dataset/normalized_refugees_dataset.csv"
 
 class PolicyChange:
     def __init__(self):
-        self.number_of_months = 3
+        self.number_of_months = 12
         self.load_policy_change_graphs()
 
     def load_policy_change_graphs(self):
@@ -28,7 +28,8 @@ class PolicyChange:
             self.remove_small_outflow_all()
             print("\tDone!!!")
         else:
-            print("Start building temporal policy graphs using %s months to compute the expected distribution" % self.number_of_months)
+            print(
+            "Start building temporal policy graphs using %s months to compute the expected distribution" % self.number_of_months)
             self.policy_graphs = self.get_policy_change_graphs(self.number_of_months)
             print("\tDone!!!")
             print("Removing noise")
@@ -39,15 +40,16 @@ class PolicyChange:
             print("\tDone!!!")
 
     def save_policy_graphs(self):
-        pickle.dump(self.policy_graphs, open('../dataset/policy_graphs_months_%s.pkl' % self.number_of_months, 'wb'), pickle.HIGHEST_PROTOCOL)
+        pickle.dump(self.policy_graphs, open('../dataset/policy_graphs_months_%s.pkl' % self.number_of_months, 'wb'),
+                    pickle.HIGHEST_PROTOCOL)
 
-    def aggregate(self,DG_temp,DG):
+    def aggregate(self, DG_temp, DG):
         list_of_edges = DG_temp.edges()
         for edge in list_of_edges:
             if DG.has_edge(edge[0], edge[1]):
                 DG[edge[0]][edge[1]]['weight'] += DG_temp[edge[0]][edge[1]]['weight']
             else:
-                DG.add_edge(edge[0], edge[1], weight = DG_temp[edge[0]][edge[1]]['weight'])
+                DG.add_edge(edge[0], edge[1], weight=DG_temp[edge[0]][edge[1]]['weight'])
         return DG
 
     # Input: start, end
@@ -55,31 +57,31 @@ class PolicyChange:
     # Output: aggregated graph from $start to $end
     def get_aggregated_graph(self, start, end):
 
-        DG =  self.temporal_network[start[1],start[0]]
+        DG = self.temporal_network[start[1], start[0]]
         month_number_start = temporal_network.months.index(start[1])
         month_number_end = temporal_network.months.index(end[1])
 
         if start[0] == end[0]:
-            for month in range(month_number_start+1, month_number_end+1):
+            for month in range(month_number_start + 1, month_number_end + 1):
                 m = temporal_network.months[month]
                 DG_temp = self.temporal_network[m, start[0]]
-                DG = self.aggregate( DG_temp, DG)
+                DG = self.aggregate(DG_temp, DG)
 
         else:
-            for year in range(start[0],end[0]+1) :
+            for year in range(start[0], end[0] + 1):
                 if year == start[0]:
-                    for month in range(month_number_start+1, 12):
+                    for month in range(month_number_start + 1, 12):
                         m = temporal_network.months[month]
                         DG_temp = self.temporal_network[m, year]
                         DG = self.aggregate(DG_temp, DG)
 
                 elif year == end[0]:
-                    for month in range(0, month_number_end+1):
+                    for month in range(0, month_number_end + 1):
                         m = temporal_network.months[month]
                         DG_temp = self.temporal_network[m, year]
                         DG = self.aggregate(DG_temp, DG)
                 else:
-                    for month in range(0,12):
+                    for month in range(0, 12):
                         m = temporal_network.months[month]
                         DG_temp = self.temporal_network[m, year]
                         DG = self.aggregate(DG_temp, DG)
@@ -99,7 +101,7 @@ class PolicyChange:
             # Normalize by the total sum
             total = float(sum([x[1] for x in distribution]))
             if total != 0:
-                distribution = map(lambda x: (x[0], x[1]/total), distribution)
+                distribution = map(lambda x: (x[0], x[1] / total), distribution)
 
             # Add the normalized weighted according the outflow to the new graph
             for node2 in distribution:
@@ -126,7 +128,7 @@ class PolicyChange:
                 # if its negative means that we have to go back plus one year,
                 # for example imagine we are in year 2001 - January and the number of months is 14
                 # in this case we want to go at 1999 - November
-                if month_number - months < 0 :
+                if month_number - months < 0:
                     years_back = years_back + 1
 
                 month_start = temporal_network.months[month_number - months]
@@ -136,15 +138,14 @@ class PolicyChange:
                     break
                 else:
                     expected_graph = self.get_distribution_outflow(self.get_aggregated_graph(
-                                                                                            (year-years_back, month_start),
-                                                                                            (year-1 if month == "January" else year, month_end)))
-                    real_graph = self.get_distribution_outflow(self.temporal_network[month,year])
-                    policy_change_graph = self.policy_change_graph(expected_graph,real_graph)
+                        (year - years_back, month_start),
+                        (year - 1 if month == "January" else year, month_end)))
+                    real_graph = self.get_distribution_outflow(self.temporal_network[month, year])
+                    policy_change_graph = self.policy_change_graph(expected_graph, real_graph)
                     N[(month, year)] = policy_change_graph
             print("\tPolicy graphs done for year " + str(year))
 
         return N
-
 
     @staticmethod
     def policy_change_graph(graph1, graph2):
@@ -170,12 +171,12 @@ class PolicyChange:
 
         return policy_change_graph
 
-#############################################################################################################
-############################## Analyses of policy graphs ####################################################
-#############################################################################################################
-# Use the $self.number_of_months attribute to compute the expected distribution in all of the following functions,
-# unless indicated otherwise
-# Use the $self.policy_graphs attribute
+    #############################################################################################################
+    ############################## Analyses of policy graphs ####################################################
+    #############################################################################################################
+    # Use the $self.number_of_months attribute to compute the expected distribution in all of the following functions,
+    # unless indicated otherwise
+    # Use the $self.policy_graphs attribute
 
     # Output format: {"Afghanistan": 456, ...}
     @staticmethod
@@ -213,17 +214,18 @@ class PolicyChange:
         file.write("Destination,Origin,Month,Year,Day/month/year,Change\n")
         time_periods = self.policy_graphs.keys()
         for time_period in time_periods:
-                for origin in self.policy_graphs[time_period]:
-                    for destination in self.policy_graphs[time_period][origin].keys():
-                        if origin != destination:
-                            month, year = time_period
-                            day_month_year = "1/%s/%s" % (temporal_network.months.index(month) + 1, year)
-                            file.write("%s,%s,%s,%s,%s,%s\n" % (destination,
-                                                                origin,
-                                                                month,
-                                                                year,
-                                                                day_month_year,
-                                                                self.policy_graphs[time_period][origin][destination]["weight"]))
+            for origin in self.policy_graphs[time_period]:
+                for destination in self.policy_graphs[time_period][origin].keys():
+                    if origin != destination:
+                        month, year = time_period
+                        day_month_year = "1/%s/%s" % (temporal_network.months.index(month) + 1, year)
+                        file.write("%s,%s,%s,%s,%s,%s\n" % (destination,
+                                                            origin,
+                                                            month,
+                                                            year,
+                                                            day_month_year,
+                                                            self.policy_graphs[time_period][origin][destination][
+                                                                "weight"]))
         file.close()
 
     # write to file the average change and the standard deviation per country
@@ -236,44 +238,113 @@ class PolicyChange:
                 weights = []
                 weight = 0.0;
 
-                if self.policy_graphs[time_period].in_degree(country) > self.policy_graphs[time_period].out_degree(country) :
+                if self.policy_graphs[time_period].in_degree(country) > self.policy_graphs[time_period].out_degree(
+                        country):
                     predecessors = self.policy_graphs[time_period].predecessors(country)
                     for predecessor in predecessors:
                         weight += self.policy_graphs[time_period][predecessor][country]["weight"]
                         weights.append(self.policy_graphs[time_period][predecessor][country]["weight"])
 
                     average_policy_change = weight / len(predecessors)
-                    file.write("%s,%s,%s,%s,%s\n" % (country, time_period[0], time_period[1], average_policy_change, np.std(weights)))
+                    file.write("%s,%s,%s,%s,%s\n" % (
+                    country, time_period[0], time_period[1], average_policy_change, np.std(weights)))
         file.close()
-
-
 
     def write_change_per_pair_to_file_tableau_type(self):
         file = open("../output/per_pair_output/policy_change_%s_months_tableau_type.csv" % self.number_of_months, "w")
         file.write("country1,country2,Destination-Origin,month,year,Day/month/year,Change\n")
         time_periods = self.policy_graphs.keys()
         for time_period in time_periods:
-                for origin in self.policy_graphs[time_period]:
-                    for destination in self.policy_graphs[time_period][origin].keys():
-                        if origin != destination:
-                            month, year = time_period
-                            destination_origin = "%s-%s" % (destination,origin)
-                            day_month_year = "1/%s/%s" % (temporal_network.months.index(month) + 1, year)
-                            file.write("%s,%s,%s,%s,%s,%s,%s\n" % (destination,
-                                                                   origin,
-                                                                   destination_origin,
-                                                                   month,
-                                                                   year,
-                                                                   day_month_year,
-                                                                   self.policy_graphs[time_period][origin][destination]["weight"]))
-                            file.write("%s,%s,%s,%s,%s,%s,%s\n" % (origin,
-                                                                   destination,
-                                                                   destination_origin,
-                                                                   month,
-                                                                   year,
-                                                                   day_month_year,
-                                                                   self.policy_graphs[time_period][origin][destination]["weight"]))
+            for origin in self.policy_graphs[time_period]:
+                for destination in self.policy_graphs[time_period][origin].keys():
+                    if origin != destination:
+                        month, year = time_period
+                        destination_origin = "%s-%s" % (destination, origin)
+                        day_month_year = "1/%s/%s" % (temporal_network.months.index(month) + 1, year)
+                        file.write("%s,%s,%s,%s,%s,%s,%s\n" % (destination,
+                                                               origin,
+                                                               destination_origin,
+                                                               month,
+                                                               year,
+                                                               day_month_year,
+                                                               self.policy_graphs[time_period][origin][destination][
+                                                                   "weight"]))
+                        file.write("%s,%s,%s,%s,%s,%s,%s\n" % (origin,
+                                                               destination,
+                                                               destination_origin,
+                                                               month,
+                                                               year,
+                                                               day_month_year,
+                                                               self.policy_graphs[time_period][origin][destination][
+                                                                   "weight"]))
         file.close()
+
+    @staticmethod
+    def apply_threshold(policy_graph, threshold, use_all=True, sign=1):
+        new_graph = nx.DiGraph()
+        for node in policy_graph.nodes():
+            new_graph.add_node(node)
+            for neighbor in policy_graph.neighbors(node):
+                weight = policy_graph[node][neighbor]["weight"]
+                if not use_all:
+                    if sign < 0:
+                        weight = weight * -1
+                else:
+                    weight = abs(weight)
+
+                if weight > threshold:
+                    new_graph.add_edge(node, neighbor)
+
+        return new_graph
+
+    def write_global_change_to_file(self):
+        threshold = 0.01
+        thresholded_graphs = lambda use_all, sign: [
+            (t, self.apply_threshold(self.policy_graphs[t], threshold, use_all, sign))
+            for t in self.policy_graphs.keys()]
+        positive_graphs = thresholded_graphs(False, 1)
+        negative_graphs = thresholded_graphs(False, -1)
+        overall_graphs = thresholded_graphs(True, 1)
+        indegree_positive = lambda graphs: \
+            map(lambda graph:
+                (graph[0], graph[1].in_degree(graph[1].nodes()).values()),
+                graphs)
+        positive_change = indegree_positive(positive_graphs)
+        negative_change = indegree_positive(negative_graphs)
+        overall_change = indegree_positive(overall_graphs)
+
+        file = open("../output/global_change/global_change_%s_months.csv" % self.number_of_months, "w")
+        file.write("month,"
+                   "year,"
+                   "Day/Month/Year,"
+                   "overall_change_avg,"
+                   "positive_change_avg,"
+                   "negative_change_avg,"
+                   "overall_change_std,"
+                   "positive_change_std,"
+                   "negative_change_std\n")
+
+        for index in range(len(positive_change)):
+            month, year = positive_change[index][0]
+            day_month_year = "1/%s/%s" % (temporal_network.months.index(month) + 1, year)
+            file.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (month,
+                                                         year,
+                                                         day_month_year,
+                                                         np.average(overall_change[index][1]),
+                                                         np.average(positive_change[index][1]),
+                                                         np.average(negative_change[index][1]),
+                                                         np.std(overall_change[index][1]),
+                                                         np.std(positive_change[index][1]),
+                                                         np.std(negative_change[index][1])))
+        file.close()
+
+    @staticmethod
+    def test_apply_threshold():
+        new_graph = nx.DiGraph()
+        new_graph.add_weighted_edges_from([(0, 1, 0.3)])
+        new_graph.add_weighted_edges_from([(2, 1, -0.3)])
+        new_graph.add_weighted_edges_from([(3, 1, 0.3)])
+        t = PolicyChange.apply_threshold(new_graph, 0.2)
 
     @staticmethod
     #   Example: self.visualize_graph(self.temporal_network[("January",2003)])
@@ -284,6 +355,4 @@ class PolicyChange:
         plt.show()
 
 
-pg = PolicyChange()
-pg.write_average_change_and_std_per_country_to_file()
-
+PolicyChange().write_global_change_to_file()
